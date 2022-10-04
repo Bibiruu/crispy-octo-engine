22855375c2504150cd6f5101af850f06735f6ce3
Maria <so.mariagloria@gmail.com>
2022-10-04 11:07:44 +0300

    Finished work

--- src/Card.js
+++ src/Card.js
@@ -6,10 +6,14 @@ export const Card = (props) =>{
     console.log(props.number)  
     return (
         <div className='card'>
-            <img className='img'src={props.img}/> 
+          <div cassName='image'>
+            <img className='img'src={props.img}/>
+          </div>
+          <div className='text'>
             <div className='number'>{props.number}</div>     
             <div className='month'>{props.month}</div> 
-            <div className='title'>{props.title}</div>                                             
+            <div className='title'>{props.title}</div>
+          </div>                                             
        </div>
     )
 }
